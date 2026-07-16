87eb1191eca6dcbd43585462ef057410a6b82529
Anuraj Upadhyay <anurajupadhyay6@gmail.com>
2026-07-16 20:02:16 +0530

    added better support for text to html nodes

--- src/textnode.py
+++ src/textnode.py
@@ -24,21 +24,22 @@ class TextNode(object):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
 
-    def text_node_to_html_node(text_node= TextNode) -> leafNode: #rewrite
-        if text_node.text_type == TextType.PLAIN:
-            return leafNode(text_node.text, "p")
+    def text_node_to_html_node(text_node: TextNode) -> LeafNode: #the thing in front of arrow indicates the return type, although python does not enforce it. TextNode is the argument type
+        if text_node.text_type == TextType.PLAIN: # the thing in front of text_node is the object, and the thing after the dot is the attribute of that object
+            return LeafNode(text_node.text, "p")
         elif text_node.text_type == TextType.BOLD:
-            return leafNode(text_node.text, "b")
+            return LeafNode(text_node.text, "b")
         elif text_node.text_type == TextType.ITALIC:
-            return leafNode(text_node.text, "i")
+            return LeafNode(text_node.text, "i")
         elif text_node.text_type == TextType.CODE:
-            return leafNode(text_node.text, "code")
+            return LeafNode(text_node.text, "code")
         elif text_node.text_type == TextType.LINKS:
-            return leafNode(text_node.text, "a", {"href": text_node.url})
+            return LeafNode(text_node.text, "a", {"href": text_node.url})
         elif text_node.text_type == TextType.IMAGES:
-            return leafNode(text_node.text, "img", {"src": text_node.url})
+            return LeafNode(text_node.text, "img",{"src": text_node.url})
         else:
             raise ValueError("Invalid text type")
+
 
 def main():
     node = TextNode("Hello world", TextType.PLAIN, None)
