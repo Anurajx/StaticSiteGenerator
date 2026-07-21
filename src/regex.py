dc9039c33f400e78db6cc8f6f4274c7661bc558b
Anuraj Upadhyay <anurajupadhyay6@gmail.com>
2026-07-21 21:44:27 +0530

    added regex for Links and Text extraction from markdown along with tests

---
+++ src/regex.py
@@ -0,0 +1,18 @@
+import re
+
+def extract_markdown_images(text):
+    matchedSubsection = re.findall(r"!\[(.*?)\]\((.*?)\)",text)
+    return matchedSubsection
+
+def extract_markdown_links(text):
+    matchedSubsection = re.findall(r"\[(.*?)\]\((.*?)\)",text)
+    return matchedSubsection
+
+
+def main():
+    testingMicImages= "hey why are there so many links (outthere) shoudnt ![rick roll](https://i.imgur.com/aKaOqIh.gif) they be (inside) there homes at this ![rick roll](https://i.imgur.com/aKaOqIh.gif) time, links hut"
+    testingMicText=  "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    extract_markdown_images(testingMicImages)
+    extract_markdown_links(testingMicText)
+
+# main()
---
+++ src/tests/test_regex.py
@@ -0,0 +1,11 @@
+import unittest
+from regex import extract_markdown_images, extract_markdown_links
+
+
+class TestRegex(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+        #add more tests for regex Links and Texts
